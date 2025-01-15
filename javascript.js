4e5fe86d2bca33137a45ac26d974bc4c7f494bc4
DESIGNER011 <amudhapriyamp@gmail.com>
2025-01-15 13:26:06 +0530

    Project initiater

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-toggle').addEventListener('click',()=>{
+     document.querySelector('.nav-links').classList.toggle('active');
+ });
+
+document.getElementById('ContactForm').addEventListener('submit',(e)=>{   
+      e.preventDefault();
+      alert("Thank you")
+ })
+
