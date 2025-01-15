76a2ac93880f742aa816690dfd0a0e841100f4e5
DESIGNER011 <amudhapriyamp@gmail.com>
2025-01-15 14:05:46 +0530

    new-comment

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 document.querySelector('.menu-toggle').addEventListener('click',()=>{
      document.querySelector('.nav-links').classList.toggle('active');
  });
-
+//hai
 document.getElementById('ContactForm').addEventListener('submit',(e)=>{   
       e.preventDefault();
       alert("Thank you")
